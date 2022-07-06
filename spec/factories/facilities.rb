2e75a683d271901269ec9da631a5f577b83e2696
Sokly <hengsokly23@gmail.com>
2022-07-06 14:47:34 +0700

    Facility: add scaffold with layout

--- spec/factories/facilities.rb
+++ spec/factories/facilities.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-#
-# Table name: facilities
-#
-#  id             :string           not null, primary key
-#  name_en        :string
-#  name_km        :string
-#  abbr_en        :string
-#  abbr_km        :string
-#  parent_id      :string
-#  lft            :integer
-#  rgt            :integer
-#  depth          :integer          default(0)
-#  children_count :integer          default(0)
-#  dataset        :string
-#  default        :boolean          default(FALSE)
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#
 FactoryBot.define do
   factory :facility do
     id { "" }
--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-#
-# Table name: facilities
-#
-#  id             :string           not null, primary key
-#  name_en        :string
-#  name_km        :string
-#  abbr_en        :string
-#  abbr_km        :string
-#  parent_id      :string
-#  lft            :integer
-#  rgt            :integer
-#  depth          :integer          default(0)
-#  children_count :integer          default(0)
-#  dataset        :string
-#  default        :boolean          default(FALSE)
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#
 require "rails_helper"
 
 RSpec.describe Facility, type: :model do
